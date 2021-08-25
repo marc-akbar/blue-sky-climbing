a18166713ba3b584cbf2fceb26409fd1e04808a1
Akbar <akbarm@AKBARM20395-OSX.local>
2021-08-25 17:28:11 -0600

    Move Map to hook:

--- src/App.js
+++ src/App.js
@@ -2,9 +2,17 @@ import React, { Component } from "react";
 import Map from "./Map";
 import "./App.css";
 
+const defaultProps = {
+  center: {
+    lat: 40.014984,
+    lng: -105.270546,
+  },
+  zoom: 14,
+};
+
 class App extends Component {
   render() {
-    return <Map />;
+    return <Map defaultProps={defaultProps} />;
   }
 }
 
